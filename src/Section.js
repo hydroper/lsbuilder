29abf9bb35f06dc57ea9eb4bd61363b952636da3
hydroper <matheusdiasdesouzads@gmail.com>
2024-03-19 10:33:22 -0300

    Update

--- src/Section.js
+++ src/Section.js
@@ -2,11 +2,13 @@ import { SectionFile } from "./SectionFile.js";
 import { SectionNumber } from "./SectionNumber.js";
 import * as htmlparser2 from "htmlparser2";
 import { DomUtils } from "htmlparser2";
+import serializeDOM from "dom-serializer";
+import assert from "node:assert";
 
 const headingTags = ["h1", "h2", "h3", "h4", "h5", "h6"];
 
 function getHeadingTagNumber(name) {
-    return Number(name.charAt(1));
+    return name.charCodeAt(1) - 0x30;
 }
 
 export class Section {
@@ -19,6 +21,7 @@ export class Section {
      */
     title;
     /**
+     * HTML content.
      * @type {string}
      */
     content;
@@ -43,8 +46,13 @@ export class Section {
     /**
      * @param {SectionFile[]} sectionFiles
      * @param {SectionNumber} sectionNumberArgument
+     * @returns {Section[]}
      */
     static processList(sectionFiles, sectionNumberArgument) {
+        /**
+         * @type {Section[]}
+         */
+        const result = [];
         const sectionNumber = sectionNumberArgument.clone();
 
         for (const file of sectionFiles) {
@@ -61,31 +69,64 @@ export class Section {
                 }
             }
 
+            // Initial section number
+            const currentSectionNumber = sectionNumber.clone();
+
+            // Initial heading tag number
+            let currentHeadingTagNumber = 1;
+
             // Build sections
             for (let i = 0; i < headingTitles.length; i++) {
+                // Track previous section number
+                let previousSectionNumber = currentSectionNumber.clone();
+
+                // Track previous heading tag number
+                let previousHeadingTagNumber = currentHeadingTagNumber;
+
                 const headingTitle = headingTitles[i];
 
                 // Delimit content
-                let content = null;
+                let domContent = null;
                 let nextHeadingTitle = i + 1 < headingTitles.length ? headingTitles[i + 1] : null;
                 const headingTitleParentIndex = domChildren.indexOf(headingTitle);
                 if (nextHeadingTitle != null) {
                     const nextHeadingTitleParentIndex = domChildren.indexOf(nextHeadingTitle);
-                    content = domChildren.slice(headingTitleParentIndex + 1, nextHeadingTitleParentIndex);
+                    domContent = domChildren.slice(headingTitleParentIndex + 1, nextHeadingTitleParentIndex);
                 } else {
-                    content = domChildren.slice(headingTitleParentIndex + 1);
+                    domContent = domChildren.slice(headingTitleParentIndex + 1);
                 }
+                const content = serializeDOM(domContent);
 
                 // Determine section number
-                const tagNumber = getHeadingTagNumber(DomUtils.getName(headingTitle));
-                const sectionNumber1 = sectionNumber.clone();
-                for (let j = sectionNumber1.values.length; j < tagNumber; j++) {
-                    sectionNumber1.push(1);
+                currentHeadingTagNumber = getHeadingTagNumber(DomUtils.getName(headingTitle));
+                if (currentHeadingTagNumber > previousHeadingTagNumber) {
+                    for (let j = previousHeadingTagNumber; j < currentHeadingTagNumber; j++) {
+                        currentSectionNumber.push(1);
+                    }
+                } else {
+                    for (let j = previousHeadingTagNumber; j-- > currentHeadingTagNumber;) {
+                        currentSectionNumber.values.pop();
+                    }
+                    currentSectionNumber.increment();
                 }
+
+                // Contribute section
+                const section = new Section(currentSectionNumber.clone(), DomUtils.innerText(headingTitle), content);
+                let correctSubsectionList = result;
+                for (let i = 1; i < currentHeadingTagNumber; i++) {
+                    assert(correctSubsectionList.length != 0, "Section \"" + section.title + "\" (" + section.number.toString() + ") must have a parent section.");
+                    correctSubsectionList = correctSubsectionList[correctSubsectionList.length - 1].subsections;
+                }
+                correctSubsectionList.push(section);
+
+                // Increment section number
+                currentSectionNumber.increment();
             }
 
             // Increment section number
             sectionNumber.increment();
         }
+
+        return result;
     }
 }
